292b4f39049e21b6cbd399cf9a99fb8b959c17fc
Angela Laverde <angelalaverde97@gmail.com>
2025-05-16 12:19:49 -0500

    Subo Main

--- src/com/miproyecto/agendatelefonica/app/Main.java
+++ src/com/miproyecto/agendatelefonica/app/Main.java
@@ -1,10 +1,66 @@
 package com.miproyecto.agendatelefonica.app;
 
+import com.miproyecto.agendatelefonica.service.Agenda;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Agenda1: añadirContacto(Contacto c), existeContacto(Contacto c), listarContacto(), buscarContacto(String nombre)
+        //Agenda2: eliminarContacto(Contacto c), modificarTelefono(String nombre, String apellido, String nuevoTelefono), agendaLlena(), espacioLibres()
+        Scanner scanner = new Scanner(System.in);
+        Agenda agenda = new Agenda();
 
-        
+        int opcion;
+
+        System.out.println("BIENVENIDO A TU AGENDA TELEFÓNICA");
+
+        do{
+            System.out.println("\n--- MENÚ ---");
+            System.out.println("1. Añadir nuevo contacto");
+            System.out.println("2. Saber si existe un contacto");
+            System.out.println("3. Buscar un contacto");
+            System.out.println("4. Eliminar un contacto");
+            System.out.println("5. Modificar teléfono de contacto");
+            System.out.println("6. Mostrar lista de contactos");
+            System.out.println("7. Mostrar cupo en la agenda");
+            System.out.println("8. Salir");
+            System.out.println("Selecciona una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion){
+                case 1:
+                    agenda.agregarContacto();
+                    break;
+                case 2:
+                    agenda.existeContacto();
+                    break;
+                case 3:
+                    agenda.buscaContacto();
+                    break;
+                case 4:
+                    agenda.eliminarContacto();
+                    break;
+                case 5:
+                    agenda.modificarTelefono();
+                    break;
+                case 6:
+                    agenda.listarContactos();
+                    break;
+                case 7:
+                    agenda.llena();
+                    agenda.espacioLibres();
+                    break;
+                case 8:
+                    System.out.println( "!Hasta luego! 😊");
+                    break;
+                default:
+                    System.out.println("Opción invalida. Intenta nuevamente.");
+            }
+        }while(opcion != 7);
+        scanner.close();
     }
 }
