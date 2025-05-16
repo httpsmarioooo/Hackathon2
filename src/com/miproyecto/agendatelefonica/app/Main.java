1af3fe99556083d63bd840c3f023db8d67190a9a
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-16 11:44:57 -0500

    ignore

--- src/com/miproyecto/agendatelefonica/app/Main.java
+++ src/com/miproyecto/agendatelefonica/app/Main.java
@@ -1,10 +1,17 @@
 package com.miproyecto.agendatelefonica.app;
+import com.miproyecto.agendatelefonica.view.PantallaInicio;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Agenda Telefónica");
+        primaryStage.setScene(PantallaInicio.crearPantallaInicio(primaryStage));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-
-        
+        launch(args);
     }
 }
