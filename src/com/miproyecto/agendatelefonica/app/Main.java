d7b68ebfd1b03d31715a21ef511c9bb2a9e90f8f
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-16 12:33:39 -0500

    update

--- src/com/miproyecto/agendatelefonica/app/Main.java
+++ src/com/miproyecto/agendatelefonica/app/Main.java
@@ -1,17 +1,7 @@
 package com.miproyecto.agendatelefonica.app;
-import com.miproyecto.agendatelefonica.view.PantallaInicio;
 import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Agenda Telefónica");
-        primaryStage.setScene(PantallaInicio.crearPantallaInicio(primaryStage));
-        primaryStage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
     }
 }
