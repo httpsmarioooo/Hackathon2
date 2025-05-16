2917a1cd9a3b1ed7309410f84647de67e205136e
Anderzon-1989 <richardanderzonchavez@gmail.com>
2025-05-16 10:47:24 -0500

    Contactos

--- src/com/miproyecto/agendatelefonica/model/Contacto.java
+++ src/com/miproyecto/agendatelefonica/model/Contacto.java
@@ -1,4 +1,59 @@
 package com.miproyecto.agendatelefonica.model;
 
+import java.util.Objects;
+
 public class Contacto {
+    private String nombre;
+    private String apellido;
+    private int telefono;
+
+    public Contacto(String nombre, String apellido, int telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + " | Apellido: " + apellido + " | Número de teléfono: " + telefono;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contacto otro = (Contacto) o;
+        return nombre.equalsIgnoreCase(otro.nombre) &&
+                apellido.equalsIgnoreCase(otro.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre.toLowerCase(), apellido.toLowerCase());
+    }
 }
